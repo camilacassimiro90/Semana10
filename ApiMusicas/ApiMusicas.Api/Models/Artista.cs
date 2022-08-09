5d35d557ccd834623facdfd214d6a91be4dfcab9
Cami <camilacassimiro@gmail.com>
2022-08-09 15:08:51 -0300

    enviei ex 04

---
+++ ApiMusicas/ApiMusicas.Api/Models/Artista.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiMusicas.Api.Models;
+
+public class Artista
+{
+  public int Id { get; set; }
+
+  public string Nome { get; set; }
+
+  public string NomeArtistico { get; set; }
+
+  public string FotoUrl { get; set; }
+
+  public string PaisOrigem { get; set; }
+}
